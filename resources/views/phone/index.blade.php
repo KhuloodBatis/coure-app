461bf274fa40d56f2c416d62f7862264e1242cc7
khulood batis <khuloodbatis@gmail.com>
2022-03-26 12:03:31 +0300

    first

---
+++ resources/views/phone/index.blade.php
@@ -0,0 +1,9 @@
+@foreach ($phoneNumbers as $phoneNumber)
+    <div class="phone-number">
+
+        <h4>{{ $phoneNumber->phone_number }}</h4>
+        <p>Belongs to {{ $phoneNumber->user()->first()->name }}</p>
+        <p>Belongs to {{ $phoneNumber->user->name }}</p>
+
+    </div>
+@endforeach
